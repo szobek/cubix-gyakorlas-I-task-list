ac1f728cc5940cad7386dde704f89a574a74f2c6
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-24 18:26:46 +0200

    add NavComponent to app-root imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./components/nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cubix-gyakorlas-I-task-list';
+  
 }
