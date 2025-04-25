da8901e5494144ad8e69ed131c1d886c4dbc2d43
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-25 14:32:01 +0200

    refactor auth guard: implement navigation to login for unauthenticated users

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,14 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService= inject(AuthService);
-  return authService.isLogged
+  const router= inject(Router);
+  if(!authService.isLogged){
+    router.navigate(['/login']);
+    return false;
+  }
+  
+  return true
 };
