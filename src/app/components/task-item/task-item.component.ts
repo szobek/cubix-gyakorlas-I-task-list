1bfb3d38e8d1b3a5f14fc4d7dce4287b0303db7d
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-25 16:58:18 +0200

    feat(task-item): add delete functionality and integrate delete SVG icon

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, Input, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { Task } from '../../models/task';
 import { TaskService } from '../../services/task.service';
 import { CommonModule } from '@angular/common';
@@ -10,20 +16,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './task-item.component.scss',
 })
 export class TaskItemComponent {
-  private _task?:any
+  private _task?: any;
   taskService = inject(TaskService);
-  @Input({required: true}) set task(task: Task) {
+  @Input({ required: true }) set task(task: Task) {
     console.log('task', task);
-    
-    this._task = task
+
+    this._task = task;
   }
- get task(): Task | undefined {
-    return this._task
- }
+  get task(): Task | undefined {
+    return this._task;
+  }
   completeTask(task: Task) {
     this.taskService.completeTask(task);
   }
   inCompleteTask(task: Task) {
     this.taskService.inCompleteTask(task);
+  }
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task);
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -11,24 +11,30 @@ export class TaskService {
   completeTask(task: Task) {
     this.incompletedTasks.update((tasks) => {
       const index = this.findTaskIndex(task.id, this.incompletedTasks);
-      console.log(tasks[index], "index of task to be completed");
-      
+      console.log(tasks[index], 'index of task to be completed');
+
       if (index !== -1) {
         tasks[index].completed = true;
-        this.completedTasks.update((completedTasks) => [...completedTasks, tasks[index]]);
+        this.completedTasks.update((completedTasks) => [
+          ...completedTasks,
+          tasks[index],
+        ]);
         tasks.splice(index, 1);
         this.saveTasks();
       }
       return tasks;
     });
   }
-  
+
   inCompleteTask(task: Task) {
     this.completedTasks.update((tasks) => {
-      const index = this.findTaskIndex(task.id,this.completedTasks);
+      const index = this.findTaskIndex(task.id, this.completedTasks);
       if (index !== -1) {
         tasks[index].completed = false;
-        this.incompletedTasks.update((incompletedTasks) => [...incompletedTasks, tasks[index]]);
+        this.incompletedTasks.update((incompletedTasks) => [
+          ...incompletedTasks,
+          tasks[index],
+        ]);
         tasks.splice(index, 1);
         this.saveTasks();
       }
@@ -37,7 +43,7 @@ export class TaskService {
   }
 
   constructor() {
-    this.loadTasks()
+    this.loadTasks();
   }
   private findTaskIndex(id: number, obj: WritableSignal<Task[]>): number {
     return obj().findIndex((task) => task.id === id);
@@ -52,8 +58,8 @@ export class TaskService {
   }
   private saveTasks() {
     const tasks = [...this.incompletedTasks(), ...this.completedTasks()];
-    console.log(tasks,"saved tasks");
-    
+    console.log(tasks, 'saved tasks');
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
@@ -61,8 +67,37 @@ export class TaskService {
     const tasks = localStorage.getItem('tasks');
     if (tasks) {
       const parsedTasks: Task[] = JSON.parse(tasks);
-      this.incompletedTasks.update((tasks) => [...tasks, ...parsedTasks.filter((task) => !task.completed)]);
-      this.completedTasks.update((tasks) => [...tasks, ...parsedTasks.filter((task) => task.completed)]);
+      this.incompletedTasks.update((tasks) => [
+        ...tasks,
+        ...parsedTasks.filter((task) => !task.completed),
+      ]);
+      this.completedTasks.update((tasks) => [
+        ...tasks,
+        ...parsedTasks.filter((task) => task.completed),
+      ]);
+    }
+  }
+
+  deleteTask(task: Task) {
+    if (task.completed) {
+      const index = this.findTaskIndex(task.id, this.completedTasks);
+      this.completedTasks.update((tasks) => {
+        if (index !== -1) {
+          tasks.splice(index, 1);
+          this.saveTasks();
+        }
+        return tasks;
+      });
+      
+    } else {
+      const index = this.findTaskIndex(task.id, this.incompletedTasks);
+      this.incompletedTasks.update((tasks) => {
+        if (index !== -1) {
+          tasks.splice(index, 1);
+          this.saveTasks();
+        }
+        return tasks;
+      });
     }
   }
 }
