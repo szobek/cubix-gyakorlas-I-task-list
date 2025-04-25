973ba7c17c88b8145720b164ebdda4b89c76effe
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-25 14:58:12 +0200

    refactor routes: add titles to home, create, and login paths for better navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'home', canActivate: [authGuard], loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
-    {path: 'create',canActivate: [authGuard], loadComponent: () => import('./components/create-task/create-task.component').then(m => m.CreateTaskComponent)},
-    {path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)},
+    {path: 'home',title: 'Home', canActivate: [authGuard], loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
+    {path: 'create', title: 'Create task', canActivate: [authGuard], loadComponent: () => import('./components/create-task/create-task.component').then(m => m.CreateTaskComponent)},
+    {path: 'login', title: 'Login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)},
 ];
